31d88b43ca26918a9d49909a0e6b974220afff50
Pedi <75835901+pedivillano@users.noreply.github.com>
2024-07-10 16:10:22 +0800

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,5 +6,4 @@ set -x
 
 echo "hello world"
 npm i -g react-native-cli
-react-native bundle --platform android --dev false --entry-file index.js --bundle-output index.android.bundle --sourcemap-output index.android.bundle.map
 pwd
