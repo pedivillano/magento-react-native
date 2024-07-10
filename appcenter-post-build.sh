6eb5302039cb5f036b07c07362219cd546d3772a
Pedi <75835901+pedivillano@users.noreply.github.com>
2024-07-10 15:59:58 +0800

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -5,9 +5,9 @@ set -e
 set -x
 
 echo "hello world!2"
+
 npm list -g react-native-cli
-npm i -g  react-native-cli
-npm install
+
 react-native bundle --platform android --dev false --entry-file index.js --bundle-output index.android.bundle --sourcemap-output index.android.bundle.map
-cd-
+
 pwd
